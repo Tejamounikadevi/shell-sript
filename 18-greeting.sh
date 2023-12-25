1ee16de76002d2e5f64d47bd60d86f3342685298
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-25 22:41:48 +0530

    roboshop shell scripts

---
+++ 18-greeting.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAME=""
+WISHES="Good Morning"
+
+USAGE(){
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. (Optional). Default is Good Morning"
+    echo " -h, Display Help and exit"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;
+        h) USAGE; exit;;
+    esac
+done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+    echo "ERROR: Both -n and -w are mandatory options."
+    USAGE
+    exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning Shell Script."
