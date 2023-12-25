7cd26566b7aabcec8f57af86bfa12d0d45e5b8c4
tejamounikadevi <tejamounikadevi@gmail.com>
2023-12-25 20:31:19 +0530

    roboshop shell scripts

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -10,10 +10,12 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage\n"
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" tejamounikadevi@gmail.com
+#echo "$message" | mail -s "High Disk Usage" tejamounikadevi@gmail.com
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "tejamounikadevi@gmail.com" "ALERT High Disk Usage"
